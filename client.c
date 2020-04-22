d31a7157af92fa1c2352e80f3a4eb696f48d1d9b
Audrey Morin <amorin@seattleu.edu>
2020-04-22 16:32:59 -0700

    message

--- client.c
+++ client.c
@@ -8,7 +8,7 @@
    
 int main(int argc, char const *argv[]) 
 { 
-    //testhello1
+    //testhello222
     int sock = 0, valread; 
     struct sockaddr_in serv_addr; 
     char *hello = "Hello from client"; 
