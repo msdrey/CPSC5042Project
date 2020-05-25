102bccd6f068e4d1179d974b76f5b5f3657c46db
Noah Glusenkamp <noah.glusenkamp@gmail.com>
2020-05-25 13:02:29 -0700

    add comments to imports

--- Server.cpp
+++ Server.cpp
@@ -1,15 +1,24 @@
-// Server side C/C++ program to demonstrate Socket programming 
+/*
+ *  Server side C/C++ program to demonstrate Socket programming 
+ * 	Group project for Seattle University class CPSC 5042 Operating Systems, Spring 2020
+ *  Group members: Audrey Morin, Ken Comvandee, Noah Glusenkamp
+ */
+
+// C dependencies
 #include <unistd.h> 
 #include <stdio.h> 
 #include <sys/socket.h> 
 #include <stdlib.h> 
 #include <netinet/in.h> 
-#include <string.h> 
+
+// C++ dependencies
+#include <iostream> 
+#include <string>
+
+// Project files
 #include "Network.h"
 #include "GameSession.h"
 
-#include <iostream> 
-#include <string>
 using namespace std;
 
 int main(int argc, char const *argv[]) 
