a80046b8243a871282d2fcbfa5eed94bbfd8e969
Audrey Morin <amorin@seattleu.edu>
2020-05-29 17:13:59 -0700

    created helper file

---
+++ InterfaceManager.cpp
@@ -0,0 +1,97 @@
+#include "InterfaceManager.h"
+
+void display(string str, string color) {//add color param
+    map<string, string> colorCode;
+    colorCode["red"] = "31";
+    colorCode["green"] = "32";
+    colorCode["yellow"] = "33";
+    colorCode["blue"] = "34";
+    colorCode["magenta"] = "35";
+    colorCode["cyan"] = "36";
+    colorCode["white"] = "37";
+    string colorCommand = "\033[0;" + colorCode[color] + "m";
+    cout << colorCommand << str << "\033[0m" << endl;
+}
+
+string takeInput() {
+    string ans;
+    getline(cin, ans);
+    return ans;
+}
+
+string takeNewWord(){
+    clearScreen();
+    string userWord;
+    string userHint;
+    display("Enter a word you want to add(with no space): ", "cyan");
+    userWord = takeInput();
+    display("Enter hint for the word: ", "cyan");
+    userHint = takeInput();
+    return userWord + "," + userHint;
+}
+
+// prompt the user for a username and password and send it to server
+string promptUserAuthentication() {
+    string choice;
+    bool validInput = false;
+    while(!validInput) {
+        display("Please enter 'sign up' if you are a new user or 'log in' if you are a returning user", "yellow");
+        choice = takeInput();
+        if ((!isAMatch(choice, "log in"))&&(!isAMatch(choice, "sign up"))) {
+            display("This is not a valid choice. Try again.", "red");
+        } else {
+            validInput = true;
+        }
+    }
+
+    display("Please enter your username: ", "yellow");
+    string username = takeInput();
+    display("Please enter your password: ", "yellow");
+    string password = takeInput();
+
+    return serializeAuthString(choice, username, password);
+}
+
+void clearScreen() {
+    system("clear");
+}
+
+string serializeAuthString(string choice, string username, string password) {
+    string result = choice + ";";
+    result += "username=" + username;
+    result += ",password=" + password;
+    return result;
+}
+
+bool isAMatch(const string& str1, const string& str2) {
+    unsigned int len = str1.length();
+    if (str2.length() != len){
+        return false;
+    }
+    for (unsigned int i = 0; i < len; ++i) {
+        if (tolower(str1[i]) != tolower(str2[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void pressAnyKeyRoutine() {
+    display("Press any key to continue.", "yellow");
+    takeInput();
+}
+
+string errorMessage(int authStatus) {
+    switch(authStatus){
+        case -1: 
+            return "Incorrect username. Disconnecting...";
+        case -2: 
+            return "Incorrect password. Disconnecting...";     
+        case -3: 
+            return "User already exists. Disconnecting...";
+        case -4: 
+            return "User is already logged in. Disconnecting...";
+        default:
+            return "auth error";
+    }
+}
---
+++ InterfaceManager.h
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+const string LOGO = 
+    " __          __           _                                       \n"
+    " \\ \\        / /          | |                                      \n"
+    "  \\ \\  /\\  / /__  _ __ __| | __ _ ___  __ _ _   _ _ __ _   _ ___  \n"
+    "   \\ \\/  \\/ / _ \\| '__/ _` |/ _` / __|/ _` | | | | '__| | | / __| \n"
+    "    \\  /\\  / (_) | | | (_| | (_| \\__ \\ (_| | |_| | |  | |_| \\__ \\ \n"
+    "     \\/  \\/ \\___/|_|  \\__,_|\\__,_|___/\\__,_|\\__,_|_|   \\__,_|___/ \n";
+
+
+
+//unit methods
+void display(string str, string color = "white");
+void clearScreen();
+string takeInput();
+string serializeAuthString(string, string, string);
+
+
+//routine methods
+string takeNewWord();
+string promptUserAuthentication();
+void pressAnyKeyRoutine();
+
+//helper 
+bool isAMatch(const string&, const string&);
+string errorMessage(int);
