e9ef88621259bc6ed0b3129c3f8227e85b1c7b6b
Audrey Morin <amorin@seattleu.edu>
2020-06-03 16:14:22 -0700

    inline comments in clientmain.cpp

---
+++ Client.h
@@ -0,0 +1,34 @@
+/**
+ * Client side C/C++ program to demonstrate Socket programming 
+ * 
+ * Audrey Morin, Noah Glusenkamp, Nitid Comvandee
+ */
+
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <arpa/inet.h> 
+#include <unistd.h> 
+#include <iostream> 
+#include <string>
+
+using namespace std;
+
+#define PORT 12119 //audrey's port on cs1 for cpsc5042
+#define AWS_IP "54.91.202.143" // our aws server's IP address
+#define LOCAL_IP "127.0.0.1"
+
+class Client {
+public:
+    int clientSocket;
+
+    Client(int, char const*argv[]);
+
+    string receiveFromServer();
+    void sendToServer(string);
+    void closeSocket();
+};
+
+
+
+#endif //CLIENT_H
---
+++ clientmain.cpp
@@ -0,0 +1,74 @@
+/**
+ *
+ */
+
+#include "Client.h"
+#include "InterfaceManager.h"
+
+int main(int argc, char const *argv[]) {    
+    try {
+        //initializeing client connection with the server
+        Client * client = new Client(argc - 1, argv);
+
+        //authentication input routine
+        clearScreen();
+        displayLogo();
+        client->sendToServer(promptUserAuthentication());
+                
+        //receive authentication result and check if valid, if not, disconnect
+        int authResult = stoi(client->receiveFromServer());
+        if (authResult < 0) {
+            display(errorMessage(authResult), "red");
+            client->closeSocket();
+            exit(0);
+        }
+        display("Your log in was successful", "green");
+        client->sendToServer("true");
+        pressAnyKeyRoutine();
+
+        //receive and display welcome message
+        clearScreen();
+        displayLogo();
+        string response = client->receiveFromServer();
+        int clearPos = response.find("@");
+        display(response.substr(0, clearPos), "green");
+        pressAnyKeyRoutine();
+
+        //display first prompt
+        clearScreen();
+        display(response.substr(clearPos + 1), "green");
+    
+        //keep playing as long as the player does not issue the command ".exit"
+        string userInput;
+        do {
+            //take in user's input and send to server
+            userInput = takeInput();
+            client->sendToServer(userInput);
+
+            //if user is adding a word, take that in and send it
+            if (isAMatch(userInput, ".addword")) {
+                response = client->receiveFromServer();
+                client->sendToServer(takeNewWord());
+            }
+
+            //receive and display feedback from server
+            response = client->receiveFromServer();
+            int clearPos = response.find("@");
+            display(response.substr(0, clearPos), "green");
+            pressAnyKeyRoutine();
+
+            //display next prompt
+            clearScreen();
+            display(response.substr(clearPos + 1), "green");
+
+        } while(!isAMatch(userInput, ".exit"));
+
+        client->closeSocket();
+
+    } catch (const char* message) {
+        cerr << message << endl;
+        exit(EXIT_FAILURE);
+    } 
+    
+    return 0; 
+} 
