b4980e8d8c9e78d971d736d6c96a82843b7c9ed8
Audrey Morin <amorin@seattleu.edu>
2020-06-03 16:07:52 -0700

    created a Client.cpp class with a separate clientmain.cpp

--- Client.cpp
+++ Client.cpp
@@ -1,21 +1,4 @@
-/**
- * Client side C/C++ program to demonstrate Socket programming 
- * 
- * Audrey Morin, Noah Glusenkamp, Nitid Comvandee
- */
-
-#include <arpa/inet.h> 
-#include <unistd.h> 
-#include <iostream> 
-#include <string>
-
-#include "InterfaceManager.h"
-
-using namespace std;
-
-#define PORT 12119 //audrey's port on cs1 for cpsc5042
-#define AWS_IP "54.91.202.143" // our aws server's IP address
-#define LOCAL_IP "127.0.0.1"
+#include "Client.h"
 
 /**
  * Establishes connection with the server according to input parameters.
@@ -31,7 +14,7 @@ using namespace std;
  * @throw an error if converting the address fails
  * @throw an error if connecting to the server fails
  */
-int create_connection(int numberOfParameters, char const *argv[]) {
+Client::Client(int numberOfParameters, char const *argv[]) {
     string hostname;
     int port;
 
@@ -76,7 +59,7 @@ int create_connection(int numberOfParameters, char const *argv[]) {
 		throw "Connecting the socket to the address failed. The server might be down."; 
     } 
 
-    return sock;
+    this->clientSocket = sock;
 }
 
 /**
@@ -87,9 +70,9 @@ int create_connection(int numberOfParameters, char const *argv[]) {
  * @return the message in the form of a string.
  * @throw an error if the receiving fails
  */
-string receiveFromServer(int sock) {
+string Client::receiveFromServer() {
     char message[1024] = {0};
-    int valread = recv(sock, message, 1024, 0);
+    int valread = recv(this->clientSocket, message, 1024, 0);
     if (valread == -1) {
 		throw "receiving error";
 	}
@@ -104,74 +87,13 @@ string receiveFromServer(int sock) {
  * @param message the message to be sent
  * @throw an error if the send fails.
  */
-void sendToServer(int sock, string message) {
-    int valsend = send(sock, message.c_str(), message.length(), 0);
+void Client::sendToServer(string message) {
+    int valsend = send(this->clientSocket, message.c_str(), message.length(), 0);
     if (valsend == -1) {
         throw "error occured while sending data to server";
     }
 }
 
-/**
- *
- */
-int main(int argc, char const *argv[]) {    
-    try {
-        //establishing connection with the server
-        int sock = create_connection(argc - 1, argv);
-
-        clearScreen();
-        displayLogo();
-        sendToServer(sock, promptUserAuthentication());
-                
-        //receive authentication result and check if valid, if not, disconnect
-        int authResult = stoi(receiveFromServer(sock));
-        if (authResult < 0) {
-            display(errorMessage(authResult), "red");
-            close(sock);
-            exit(0);
-        }
-        display("Your log in was successful", "green");
-        sendToServer(sock, "true");
-        pressAnyKeyRoutine();
-
-        //receive and display welcome message & prompt
-        clearScreen();
-        displayLogo();
-        string response = receiveFromServer(sock);
-        int clearPos = response.find("@");
-        display(response.substr(0, clearPos), "green");
-        pressAnyKeyRoutine();
-        clearScreen();
-        display(response.substr(clearPos + 1), "green");
-    
-        //keep playing as long as the player does not issue the command ".exit"
-        string userInput;
-        do {
-            //take in user's input and send to server
-            userInput = takeInput();
-            sendToServer(sock, userInput);
-
-            if (isAMatch(userInput, ".addword")) {
-                response = receiveFromServer(sock);
-                sendToServer(sock, takeNewWord());
-            }
-
-            //receive feedback + next prompt from server, and display them
-            response = receiveFromServer(sock);
-            int clearPos = response.find("@");
-            display(response.substr(0, clearPos), "green");
-            pressAnyKeyRoutine();
-            clearScreen();
-            display(response.substr(clearPos + 1), "green");
-
-        } while(!isAMatch(userInput, ".exit"));
-
-        close(sock);
-
-    } catch (const char* message) {
-        cerr << message << endl;
-        exit(EXIT_FAILURE);
-    } 
-    
-    return 0; 
-} 
+void Client::closeSocket() {
+    close(this->clientSocket);
+}
--- makefile
+++ makefile
@@ -6,8 +6,8 @@ all: server client
 server: ServerMain.cpp WordLibrary.cpp Server.cpp GameSession.cpp Network.cpp Connection.cpp
 	$(CC) $(CFLAGS) ServerMain.cpp WordLibrary.cpp Server.cpp GameSession.cpp Network.cpp Connection.cpp -o bin/server
 
-client: Client.cpp InterfaceManager.cpp
-	$(CC) $(CFLAGS) Client.cpp InterfaceManager.cpp -o bin/client
+client: clientmain.cpp Client.cpp InterfaceManager.cpp
+	$(CC) $(CFLAGS) clientmain.cpp Client.cpp InterfaceManager.cpp -o bin/client
 
 clean:
 	rm bin/*.o
